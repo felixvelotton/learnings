18acc41dff9dec617192a294945f0d1c0f8be963
felixvelotton <felixvn@hotmail.com>
2023-03-09 06:58:42 -0300

    flutter

---
+++ flutter_9.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+main() {
+  // Área da circunferência = PI * raio * raio
+
+  const PI = 3.1415;
+
+  stdout.write("Informe o raio: ");
+  final entradaDoUsuario = stdin.readLineSync()!;
+  final double raio = double.parse(entradaDoUsuario);
+
+  // entradaDoUsuario = "";
+
+  final area = PI * raio * raio;
+  print("O valor da área é: " + area.toString());
+}
+//Existem dois tipos de constantes 
+// const e final
+// const serve para constante a serem executadas
+// durante a compilação
+//final serve para constantes a serem lidas apos a execução
+ 
