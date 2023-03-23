27111bd43d48f92671ed991a44bfc6f45d7d6e59
felixvelotton <felixvn@hotmail.com>
2023-03-23 14:44:47 -0300

    flutter

--- flutter/flutter_13.dart
+++ flutter/flutter_13.dart
@@ -1,6 +1,24 @@
-main(){
-    var times = {'America-MG','Bragantino','Corintians','Cruzeiro','Diamantina','Palmeiras','Sao Paulo'};
-    print(times is Set);
-    times.add('Flamengo');
-    print(times.lenght);
+main() {
+  var times = {
+    'America-MG',
+    'Bragantino',
+    'Corintians',
+    'Cruzeiro',
+    'Diamantina',
+    'Palmeiras',
+    'Sao Paulo'
+  };
+  print(times
+      is Set); //A diferença entre Set e List é que Set não aceita repetição e List sim aceita
+  times.add('Flamengo'); //adiciona um novo elemento na lista
+  print(times.length); //apresenta a quantidade de elementos da lista
+  print(
+      times); //imprime a lista e mais o novo elemento inserido pela função times.add
+  print(times.contains(
+      "Palmeiras")); //consulta se existir um determinado time dentro da lista
+  print(times.contains(
+      "vasco")); // consulta se o time esta dentro do da lista caso verdadeiro
+  // é true caso falso é false.
+  print(times.first);
+  print(times.last);
 }
