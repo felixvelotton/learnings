5e6ca39b0a4c0b96503736217d049d9473e3cd76
felixvelotton <felixvn@hotmail.com>
2023-04-03 13:14:40 -0300

    for editado

---
+++ flutter/estrutura_controle/for_01.1.dart
@@ -0,0 +1,9 @@
+main() {
+  // for in ou array
+  //percorrendo uma lista com for
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+  for (var i = 0; i < notas.length; i++) {
+    print("i${i + 1}=${notas[i]}.");
+  }
+  print("Fim");
+}
