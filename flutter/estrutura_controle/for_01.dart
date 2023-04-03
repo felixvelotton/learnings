421df99e3c195c9f89a960eb53b3aded087de924
felixvelotton <felixvn@hotmail.com>
2023-04-03 13:10:07 -0300

    correcao

--- flutter/estrutura_controle/for_01.dart
+++ flutter/estrutura_controle/for_01.dart
@@ -1,26 +1,16 @@
 main() {
   for (int a = 0; a <= 10; a++) {
-    print("a = $a");
+    //print("a = $a");
   }
   for (int a = 1; a <= 10; a += 2) {
-    print('a = $a');
+    //print('a = $a');
   }
   for (int a = 1; a <= 10; a = a + 2) {
-    print('a = $a');
+    //print('a = $a');
   }
   //exercicio fazer um lazo for de 100 a 0,
   //reduzindo de 4 em 4 de forma decrecente
   for (int a = 100; a >= 0; a = a - 4) {
     print('a = $a');
   }
-}
-
-// for in ou array
-//percorrendo uma lista com for
-main() {
-  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
-  for (var i = 0; i < notas.length; i++) {
-    print("i${i + 1}=${notas[i]}.");
-  }
-  print("Fim");
 }
