f4b01a937793d8fa6d8edb8256f3ae0433607e75
felixvelotton <felixvn@hotmail.com>
2023-06-11 19:20:53 -0300

    funcao

---
+++ flutter/funcao/retornar_funcao.dart
@@ -0,0 +1,11 @@
+int Function(int) somaParcial(int a) {
+  //não esquecer de especificar tipo de função.
+  int c = 0;
+  return (int b) {
+    return a + b + c;
+  };
+}
+
+main() {
+  print(somaParcial(2)(10));
+}
