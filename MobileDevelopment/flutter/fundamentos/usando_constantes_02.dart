a587e66c1a94320abd16a48f4df1af83e4de0bb8
felixvelotton <flxvlt@gmail.com>
2024-03-29 08:52:51 -0300

    editing learnings

---
+++ MobileDevelopment/flutter/fundamentos/usando_constantes_02.dart
@@ -0,0 +1,9 @@
+main() {
+  const lista = ['Ana', 'Lia', 'Gui'];
+  //lista.add('rebeca'); quando definimos nossa lista como const não é
+  //possivel modificar a lista, porem se definimos como var é possivel
+  print(lista);
+
+  ///lista = ['Banana', 'Maça'];
+  //print(lista);
+}
---
+++ MobileDevelopment/flutter/fundamentos/usando_stdin.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+main() {
+  String texto = stdin.readLineSync()!; // cuidado: * stdin é minusculo não maiusculo
+  //é importante colocar null (!) no final senão não funciona.
+  print(texto);
+}
+
+
+ 
