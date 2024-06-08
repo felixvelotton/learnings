0f946fd82d778f350fdf69727615a04f55746588
felixvelotton <felixvn@hotmail.com>
2024-06-08 11:15:38 -0300

    parImpar

---
+++ programmingLeanguages/cpp/estructurasCondicionales/parImpar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+        int a;
+        cout<<"ingrese un numero"<<endl;cin>>a; 
+        if (a % 2 == 0){
+            cout<<"el numero é par"<<endl;
+        }
+        else {
+            cout<<"el numero é impar"<<endl;
+        }
+}
